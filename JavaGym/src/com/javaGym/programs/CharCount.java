6e7aef30c0036645cf49ea52e012b90c3ac467be
Sanjay <sanjayspear@gmail.com>
2022-08-24 03:54:07 +0530

    Still Character Count logic not completed yet

---
+++ JavaGym/src/com/javaGym/programs/CharCount.java
@@ -0,0 +1,11 @@
+package com.javaGym.programs;
+
+public class CharCount {
+
+	public static void main(String[] args) {
+		String s1 = "Hello World";
+	       String s2 = s1.trim();
+	       int length = s2.length();
+	}
+
+}
