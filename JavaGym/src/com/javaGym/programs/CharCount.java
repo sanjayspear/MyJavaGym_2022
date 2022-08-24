ca32e8984d14c50f7f064eab5a59ef9d69ab84ed
Sanjay <sanjayspear@gmail.com>
2022-08-24 11:12:13 +0530

    The file CharCount.java got implemented with char count logic successfully

--- JavaGym/src/com/javaGym/programs/CharCount.java
+++ JavaGym/src/com/javaGym/programs/CharCount.java
@@ -3,7 +3,19 @@ package com.javaGym.programs;
 public class CharCount {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		String s = "Welcome to Java";
+		int count=0;
+		
+		int len=s.length();
+		
+		for(int i=0; i<len; i++)
+		{
+			if(s.charAt(i)!=' ')
+			{
+				count++;
+			}
+		}
+		System.out.println("Total number of characters from the string ===>"+" ["+s+"] is ==>"+count);
 	}
 
 }
