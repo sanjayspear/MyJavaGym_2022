3994f1ac8e2dfccc0bacc71e3546884232f84674
Sanjay <sanjayspear@gmail.com>
2022-08-24 10:53:08 +0530

    Sample code for test

--- JavaGym/src/com/javaGym/programs/CharCount.java
+++ JavaGym/src/com/javaGym/programs/CharCount.java
@@ -3,9 +3,7 @@ package com.javaGym.programs;
 public class CharCount {
 
 	public static void main(String[] args) {
-		String s1 = "Hello World";
-	       String s2 = s1.trim();
-	       int length = s2.length();
+		System.out.println("Hello World");
 	}
 
 }
